1092a01a95cca1730c4e5d6c8fa98d5c4b16691f
Ihsan Guldur <ihsan@okuvaryum.com.tr>
2025-01-18 03:21:11 +0300

    fork

--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
+                "vimdoc", "go", "java", "python", "query", "markdown", "markdown_inline", "javascript", "typescript", "c", "lua", "rust",
                 "jsdoc", "bash",
             },
 
